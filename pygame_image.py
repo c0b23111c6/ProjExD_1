b5b2475bcd3ab66aea1d953fa86b006e452ed1bc
nishigaya <c0b23111c6@edu.teu.ac.jp>
2024-11-19 15:57:55 +0900

    6-2間延びの対処

--- pygame_image.py
+++ pygame_image.py
@@ -18,11 +18,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-tmr, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
+        if tmr>=800:
+            tmr = 0
 
 
 if __name__ == "__main__":
