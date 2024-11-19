f21b5eb16bf369bdf8a108f4c00e84898c72352c
nishigaya <c0b23111c6@edu.teu.ac.jp>
2024-11-19 16:14:22 +0900

    練習7-2

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像Surfaceを作成する
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png") #こうかとんの画像Surfaceを作成する
     kk_img = pg.transform.flip(kk_img, True, False)
 
@@ -18,14 +19,15 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0])
+        x = -(tmr%3200) # 練習6-2
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bg_img2, [x+4800, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
-        if tmr>=800:
-            tmr = 0
-
 
 if __name__ == "__main__":
     pg.init()
