bca0eeb9358d7d78579bb823f071d88463c51ed8
nishigaya <c0b23111c6@edu.teu.ac.jp>
2024-11-19 17:12:01 +0900

    演習１：左に流れる移動

--- pygame_image.py
+++ pygame_image.py
@@ -20,17 +20,17 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        key_lst = pg.key.get_pressed()
+        key_lst = pg.key.get_pressed() 
 
         if key_lst[pg.K_UP]:
             kk_rct.move_ip(0, -1)
         if key_lst[pg.K_DOWN]:
-             kk_rct.move_ip(0, +1)
-        if not key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:
             kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(1, 0)
-
+            kk_rct.move_ip(+2, 0)
+        kk_rct.move_ip(-1, 0)
 
 
         x = -(tmr%3200) # 練習6-2
