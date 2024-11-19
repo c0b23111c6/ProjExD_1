817319f4a645fc799937e4d1a8195c99d74358c0
nishigaya <c0b23111c6@edu.teu.ac.jp>
2024-11-19 15:39:56 +0900

    こうかとん画像の作成

--- pygame_image.py
+++ pygame_image.py
@@ -9,13 +9,16 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像Surfaceを作成する
+    kk_img = pg.image.load("fig/3.png") #こうかとんの画像Surfaceを作成する
+    kk_img = pg.transform.flip(kk_img, True, False)
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [100, 0])
+        screen.blit(bg_img, [0, 0])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
